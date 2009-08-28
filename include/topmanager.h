b4b86e945c8090275512f3d8c283e47da65fd8d3
Luca Abeni <luca@nowhere.science.unitn.it>
2009-08-28 10:18:23 +0200

    Reorder the declarations to make the interface more similar to http://napa-wine.eu/cgi-bin/twiki/viewauth/Main/TopologyManager

--- include/topmanager.h
+++ include/topmanager.h
@@ -1,13 +1,15 @@
 #ifndef TOPMAN_H
 #define TOPMAN_H
-int topInit(struct nodeID *myID);
-int topAddNeighbour(struct nodeID *neighbour);
-int topParseData(const uint8_t *buff, int len);
 const struct nodeID **topGetNeighbourhood(int *n);
-
 int topGrowNeighbourhood(int n);
 int topShrinkNeighbourhood(int n);
 int topRemoveNeighbour(struct nodeID *neighbour);
+
+
+int topInit(struct nodeID *myID);
+int topAddNeighbour(struct nodeID *neighbour);
+int topParseData(const uint8_t *buff, int len);
+
 /*
 //const struct peer* topGetNeighbour(PeerID?) ???
 //void topRegAddListener(void (*NewNeighbourCB)(const struct peer ))
