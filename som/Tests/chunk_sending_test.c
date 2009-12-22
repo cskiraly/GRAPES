bfd2c457937becb5a36d0b26174263f156aca6e0
Luca Abeni <luca@nowhere.science.unitn.it>
2009-12-22 15:59:38 +0100

    Chunk sending test

---
+++ som/Tests/chunk_sending_test.c
@@ -0,0 +1,122 @@
+/*
+ *  Copyright (c) 2009 Luca Abeni
+ *
+ *  This is free software; see GPL.txt
+ */
+#include <sys/select.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+
+#include "net_helper.h"
+#include "chunk.h"
+#include "trade_msg_la.h"
+#include "trade_msg_ha.h"
+#include "net_helpers.h"
+
+static const char *my_addr = "127.0.0.1";
+static int port = 6666;
+static int dst_port;
+static const char *dst_ip;
+
+#define BUFFSIZE 1024
+
+static void chunk_print(FILE *f, const struct chunk *c)
+{
+  const uint8_t *p;
+
+  fprintf(f, "Chunk %d:\n", c->id);
+  fprintf(f, "\tTS: %llu\n", c->timestamp);
+  fprintf(f, "\tPayload size: %d\n", c->size);
+  fprintf(f, "\tAttributes size: %d\n", c->attributes_size);
+  p = c->data;
+  fprintf(f, "\tPayload:\n");
+  fprintf(f, "\t\t%c %c %c %c ...:\n", p[0], p[1], p[2], p[3]);
+  if (c->attributes_size > 0) {
+    p = c->attributes;
+    fprintf(f, "\tAttributes:\n");
+    fprintf(f, "\t\t%c %c %c %c ...:\n", p[0], p[1], p[2], p[3]);
+  }
+}
+
+static struct nodeID *init(void)
+{
+  struct nodeID *myID;
+
+  myID = create_socket(my_addr, port);
+  if (myID == NULL) {
+    fprintf(stderr, "Error creating my socket (%s:%d)!\n", my_addr, port);
+  }
+  chunkInit(myID);
+
+  return myID;
+}
+
+static void cmdline_parse(int argc, char *argv[])
+{
+  int o;
+
+  while ((o = getopt(argc, argv, "p:i:P:I:")) != -1) {
+    switch(o) {
+      case 'p':
+        dst_port = atoi(optarg);
+        break;
+      case 'i':
+        dst_ip = strdup(optarg);
+        break;
+      case 'P':
+        port =  atoi(optarg);
+        break;
+      case 'I':
+        my_addr = iface_addr(optarg);
+        break;
+      default:
+        fprintf(stderr, "Error: unknown option %c\n", o);
+
+        exit(-1);
+    }
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  struct nodeID *my_sock;
+  struct chunk c;
+
+  cmdline_parse(argc, argv);
+
+  my_sock = init();
+  if (dst_port != 0) {
+    struct nodeID *dst;
+
+    c.id = 666;
+    c.timestamp = 1000000000ULL;
+    c.size = strlen("ciao") + 1;
+    c.data = (uint8_t *)"ciao";
+    c.attributes_size = 0;
+
+    dst = create_socket(dst_ip, dst_port);
+    sendChunk(dst, &c);
+  } else {
+    /* Receive a chunk and print it! */
+    int res;
+    struct nodeID *remote;
+    static uint8_t buff[BUFFSIZE];
+
+    res = recv_data(my_sock, &remote, buff, BUFFSIZE);
+    /* TODO: Error check! */
+    if (buff[0] != 12) {
+      fprintf(stderr, "Wrong message type!!!\n");
+
+      return -1;
+    }
+    res = decodeChunk(&c, buff + 1, res);
+    fprintf(stdout, "Decoding: %d\n", res);
+    chunk_print(stdout, &c);
+  }
+
+  return 0;
+}
+
