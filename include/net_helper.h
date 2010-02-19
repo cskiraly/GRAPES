0567c2b4ee8ffad153b90f9eb7bec20d206fac63
Luca Abeni <luca@nowhere.science.unitn.it>
2010-02-19 09:55:51 +0100

    Due to ML crappines, the "to" parameter of sent_to_peer() cannot be const

--- include/net_helper.h
+++ include/net_helper.h
@@ -9,7 +9,7 @@ int nodeid_equal(const struct nodeID *s1, const struct nodeID *s2);
 struct nodeID *create_node(const char *IPaddr, int port);
 void nodeid_free(struct nodeID *s);
 struct nodeID *net_helper_init(const char *IPaddr, int port);
-int send_to_peer(const struct nodeID *from, const struct nodeID *to, const uint8_t *buffer_ptr, int buffer_size);
+int send_to_peer(const struct nodeID *from, struct nodeID *to, const uint8_t *buffer_ptr, int buffer_size);
 int recv_from_peer(const struct nodeID *local, struct nodeID **remote, uint8_t *buffer_ptr, int buffer_size);
 
 int wait4data(const struct nodeID *n, struct timeval tout);
