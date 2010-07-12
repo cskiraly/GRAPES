40923828c6907c8a50538f682d9c0d41c0fc3ca2
Alessandro Russo <russo@disi.unitn.it>
2010-07-12 17:34:08 +0200

    SOM-test split signaling test in client and server side.

--- som/Tests/chunk_signaling_test.c
+++ som/Tests/chunk_signaling_test.c
@@ -20,6 +20,8 @@
 #include "trade_sig_ha.h"
 #include "chunkid_set_h.h"
 
+#define BUFFSIZE 1024
+
 static const char *my_addr = "127.0.0.1";
 static int port = 6666;
 static int dst_port;
@@ -28,7 +30,14 @@ static int random_bmap = 0;
 enum sigz { offer, request, sendbmap, reqbmap, unknown
 };
 static enum sigz sig = unknown;
-#define BUFFSIZE 1024
+static struct chunkID_set *cset = NULL;
+static struct chunkID_set *rcset = NULL;
+static struct nodeID *remote;
+static uint8_t buff[BUFFSIZE];
+static int max_deliver, trans_id, ret, chunktosend;
+static enum signaling_type sig_type;
+static struct nodeID *owner;
+
 
 static struct nodeID *init(void)
 {
@@ -86,20 +95,9 @@ static void cmdline_parse(int argc, char *argv[])
 }
 
 
-int main(int argc, char *argv[]) {
-    struct nodeID *my_sock;
-    struct chunkID_set *cset = NULL;
-    struct chunkID_set *rcset = NULL;
-    struct nodeID *remote;   
-    static uint8_t buff[BUFFSIZE];    
-    int max_deliver, trans_id, ret;
-    enum signaling_type sig_type;
-    struct nodeID *owner;
-    max_deliver = trans_id = ret = 0;
-    cmdline_parse(argc, argv);
-    my_sock = init();    
-    if (dst_port != 0) {//sender
+int client_side(struct nodeID *my_sock){//sender
     struct nodeID *dst;
+    max_deliver = trans_id = ret = 0;    
     dst = create_node(dst_ip, dst_port);    
     fprintf(stdout,"Sending signal ");
     switch(sig){
@@ -165,11 +163,17 @@ int main(int argc, char *argv[]) {
     }
     printChunkID_set(rcset);
     fprintf(stdout, "Trans_id = %d; Max_del = %d\n", trans_id,max_deliver);
-    nodeid_free(remote);
-  } else {//receiver side
-    int chunktosend;
+    if (cset) chunkID_set_free(cset);
+    if (rcset) chunkID_set_free(rcset);
+    if(owner)nodeid_free(owner);
+    nodeid_free(remote);    
+    return 1;
+  }
+
+int server_side(struct nodeID *my_sock){
+    max_deliver = trans_id = ret = 0;    
     ret = recv_from_peer(my_sock, &remote, buff, BUFFSIZE);
-    
+
     /* TODO: Error check! */
     if (buff[0] != MSG_TYPE_SIGNALLING) {
       fprintf(stderr, "Wrong message type!!!\n");
@@ -186,7 +190,7 @@ int main(int argc, char *argv[]) {
             fprintf(stdout, "2) Acceping only latest chunk #%d\n", chunktosend);
             chunkID_set_add_chunk(rcset,chunktosend);
             acceptChunks(remote,rcset,trans_id++);
-            break;        
+            break;
         case sig_request:
             fprintf(stdout, "1) Message REQUEST: peer requests %d chunks\n", chunkID_set_size(cset));
             printChunkID_set(cset);
@@ -195,7 +199,7 @@ int main(int argc, char *argv[]) {
             fprintf(stdout, "2) Deliver only earliest chunk #%d\n", chunktosend);
             chunkID_set_add_chunk(rcset,chunktosend);
             deliverChunks(remote,rcset,trans_id++);
-            break;        
+            break;
         case sig_send_buffermap:
             fprintf(stdout, "1) Message SEND_BMAP: I received a buffer of %d chunks\n", chunkID_set_size(cset));
             printChunkID_set(cset);
@@ -221,9 +225,22 @@ int main(int argc, char *argv[]) {
             break;
     }
     nodeid_free(remote);
+    if(owner)nodeid_free(owner);
+    if (cset) chunkID_set_free(cset);
+    if (rcset) chunkID_set_free(rcset);
+    return 1;
   }
-  if (cset) chunkID_set_free(cset);
-  if (rcset) chunkID_set_free(rcset);
-  nodeid_free(my_sock);
-  return 0;
+
+int main(int argc, char *argv[]) {
+    struct nodeID *my_sock;    
+    int ret;
+    cmdline_parse(argc, argv);
+    my_sock = init();
+    ret = 0;
+    if(dst_port!=0)
+        ret = client_side(my_sock);
+     else
+        ret = server_side(my_sock);//receiver side
+    nodeid_free(my_sock);
+    return ret;
 }
