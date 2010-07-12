f51823b97fd6fc798497dd483e7d02df8da9042d
Alessandro Russo <russo@disi.unitn.it>
2010-07-12 17:13:22 +0200

    Add helper functions to create/check/print cset, and use them in chunkidset_test

--- som/Tests/chunkidset_test.c
+++ som/Tests/chunkidset_test.c
@@ -10,57 +10,24 @@
 #include <string.h>
 #include "chunkidset.h"
 #include "trade_sig_la.h"
-
-static void check_chunk(struct chunkID_set *c, int n)
-{
-  printf("Is %d in the Chunk ID Set?\t", n);
-  if (chunkID_set_check(c, n) >= 0) {
-    printf("Yes\n");
-  } else {
-    printf("No\n");
-  }
-}
-
-static void add_chunk(struct chunkID_set *c, int n)
-{
-  int res;
-
-  res = chunkID_set_add_chunk(c, n);
-  printf("Add %d ---> Result=%d\tSize=%d\n", n, res, chunkID_set_size(c));
-}
-
-static void print_set(struct chunkID_set *c)
-{
-  int i, size = chunkID_set_size(c);
-
-  printf("Chunk ID Set size: %d\n", size);
-  printf("Chunks (in priority order): ");
-  for (i = 0; i < size; i++) {
-    printf(" %d", chunkID_set_get_chunk(c, i));
-  }
-
-  printf("\n");
-}
-
-static void populate(struct chunkID_set *cset)
-{
-  int i;
-
-  for(i = 1; i < 400; i+=3){
-    add_chunk(cset, (i%140) );
-  }  
-}
+#include "chunkid_set_h.h"
 
 static void simple_test(void)
 {
   struct chunkID_set *cset;
   char config[32];
+
   sprintf(config,"size=%d",10);
   cset = chunkID_set_init(config);
+  if(!cset){
+    fprintf(stderr,"Unable to allocate memory for rcset\n");
+
+    return;
+  }
 
   printf("Chunk ID Set initialised: size is %d\n", chunkID_set_size(cset));
-  populate(cset);
-  print_set(cset);
+  fillChunkID_set(cset, 0);
+  printChunkID_set(cset);
   check_chunk(cset, 4);
   check_chunk(cset, 2);
   check_chunk(cset, 3);
@@ -77,16 +44,22 @@ static void encoding_test(enum chunkID_set_encoding_type mode)
   void *meta;
   char config[32];
 
-  sprintf(config,"type=%d", mode);
+  sprintf(config, "type=%d", mode);
   cset = chunkID_set_init(config);
-  populate(cset);
+  if(!cset){
+    fprintf(stderr,"Unable to allocate memory for rcset\n");
+
+    return;
+  }
+
+  fillChunkID_set(cset, 0);
   res = encodeChunkSignaling(cset, NULL, 0, buff, sizeof(buff));
   printf("Encoding Result: %d\n", res);
   chunkID_set_clear(cset, 0);
   free(cset);
   
   cset1 = decodeChunkSignaling(&meta, &meta_len, buff, res);
-  print_set(cset1);
+  printChunkID_set(cset1);
   chunkID_set_clear(cset1, 0);
   free(cset1);
 }
