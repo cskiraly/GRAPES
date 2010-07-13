c3b1c35edcace73ebaf6212bea074362b17ba8f6
Luca Abeni <luca.abeni@unitn.it>
2010-07-13 12:28:54 +0200

    Cosmetics

--- som/Tests/chunk_signaling_test.c
+++ som/Tests/chunk_signaling_test.c
@@ -95,67 +95,74 @@ static void cmdline_parse(int argc, char *argv[])
 }
 
 
-int client_side(struct nodeID *my_sock){//sender
+int client_side(struct nodeID *my_sock)
+{
     struct nodeID *dst;
-    max_deliver = trans_id = ret = 0;    
-    dst = create_node(dst_ip, dst_port);    
+
+    max_deliver = trans_id = ret = 0;
+    dst = create_node(dst_ip, dst_port);
     fprintf(stdout,"Sending signal ");
-    switch(sig){
+    switch (sig) {
         case offer:
             cset = chunkID_set_init("size=10");
-            if(!cset){
+            if (!cset) {
                 fprintf(stderr,"Unable to allocate memory for cset\n");
+
                 return -1;
             }
-            fillChunkID_set(cset,random_bmap);
-            fprintf(stdout,"\"OFFER\" ");
-            ret = offerChunks(dst,cset,8,22);
+            fillChunkID_set(cset, random_bmap);
+            fprintf(stdout, "\"OFFER\" ");
+            ret = offerChunks(dst, cset, 8, 22);
             break;
         case request:
             cset = chunkID_set_init("size=10");
-            if(!cset){
+            if (!cset) {
                 fprintf(stderr,"Unable to allocate memory for cset\n");
+
                 return -1;
             }
-            fprintf(stdout,"\"REQUEST\" ");
+            fprintf(stdout, "\"REQUEST\" ");
             fillChunkID_set(cset,random_bmap);
             printChunkID_set(cset);
-            ret = requestChunks(dst,cset,10,44);
+            ret = requestChunks(dst, cset, 10, 44);
             break;
         case sendbmap:
             cset = chunkID_set_init("type=bitmap,size=10");
-            if(!cset){
+            if (!cset) {
                 fprintf(stderr,"Unable to allocate memory for cset\n");
+
                 return -1;
             }
-            fillChunkID_set(cset,random_bmap);
-            fprintf(stdout,"\"SEND BMAP\" ");
-            ret = sendBufferMap(dst,dst,cset,88);
+            fillChunkID_set(cset, random_bmap);
+            fprintf(stdout, "\"SEND BMAP\" ");
+            ret = sendBufferMap(dst, dst, cset, 88);
             break;
         case reqbmap:
-            fprintf(stdout,"\"REQUEST BMAP\" ");
-            ret = requestBufferMap(dst,NULL,99);
+            fprintf(stdout, "\"REQUEST BMAP\" ");
+            ret = requestBufferMap(dst, NULL, 99);
             break;
         default:
             printf("Please select one operation (O)ffer, (R)equest, send (B)map, request (b)map\n");
+
             return -1;
-    }   
-    fprintf(stdout,"done: %d\n",ret);
+    }
+    fprintf(stdout, "done: %d\n", ret);
     nodeid_free(dst);
     ret = recv_from_peer(my_sock, &remote, buff, BUFFSIZE);
     if (buff[0] != MSG_TYPE_SIGNALLING) {
       fprintf(stderr, "Wrong message type!!!\n");
+
       return -1;
     }
     fprintf(stdout,"Received message of %d bytes\n",ret);
-    ret = parseSignaling(buff+1,ret-1, &owner, &rcset, &max_deliver, &trans_id, &sig_type);    
+    ret = parseSignaling(buff+1,ret-1, &owner, &rcset, &max_deliver, &trans_id, &sig_type);
     fprintf(stdout, "Trans_id = %d; Max_del = %d\n", trans_id,max_deliver);
-    switch(sig_type){        
+    switch (sig_type){
         case sig_accept:
-            fprintf(stdout, "Accept of %d chunks\n", chunkID_set_size(rcset));            
+            fprintf(stdout, "Accept of %d chunks\n", chunkID_set_size(rcset));
             break;
         case sig_deliver:
-            fprintf(stdout, "Deliver of %d chunks\n", chunkID_set_size(rcset));            
+            fprintf(stdout, "Deliver of %d chunks\n", chunkID_set_size(rcset));
             break;
         case sig_send_buffermap:
             fprintf(stdout, "I received a buffer of %d chunks\n", chunkID_set_size(rcset));
@@ -166,17 +173,20 @@ int client_side(struct nodeID *my_sock){//sender
     if (cset) chunkID_set_free(cset);
     if (rcset) chunkID_set_free(rcset);
     if(owner)nodeid_free(owner);
-    nodeid_free(remote);    
-    return 1;
-  }
+    nodeid_free(remote);
 
-int server_side(struct nodeID *my_sock){
-    max_deliver = trans_id = ret = 0;    
+    return 1;
+}
+
+int server_side(struct nodeID *my_sock)
+{
+    max_deliver = trans_id = ret = 0;
     ret = recv_from_peer(my_sock, &remote, buff, BUFFSIZE);
 
     /* TODO: Error check! */
     if (buff[0] != MSG_TYPE_SIGNALLING) {
       fprintf(stderr, "Wrong message type!!!\n");
+
       return -1;
     }
     fprintf(stdout,"Received message of %d bytes: trans_id %d, Max_deliver %d\n",ret,trans_id,max_deliver);
@@ -188,8 +198,8 @@ int server_side(struct nodeID *my_sock){
             printChunkID_set(cset);
             rcset = chunkID_set_init("size=1");
             fprintf(stdout, "2) Acceping only latest chunk #%d\n", chunktosend);
-            chunkID_set_add_chunk(rcset,chunktosend);
-            acceptChunks(remote,rcset,trans_id++);
+            chunkID_set_add_chunk(rcset, chunktosend);
+            acceptChunks(remote, rcset, trans_id++);
             break;
         case sig_request:
             fprintf(stdout, "1) Message REQUEST: peer requests %d chunks\n", chunkID_set_size(cset));
@@ -197,50 +207,56 @@ int server_side(struct nodeID *my_sock){
             chunktosend = chunkID_set_get_earliest(cset);
             rcset = chunkID_set_init("size=1");
             fprintf(stdout, "2) Deliver only earliest chunk #%d\n", chunktosend);
-            chunkID_set_add_chunk(rcset,chunktosend);
-            deliverChunks(remote,rcset,trans_id++);
+            chunkID_set_add_chunk(rcset, chunktosend);
+            deliverChunks(remote, rcset, trans_id++);
             break;
         case sig_send_buffermap:
             fprintf(stdout, "1) Message SEND_BMAP: I received a buffer of %d chunks\n", chunkID_set_size(cset));
             printChunkID_set(cset);
             rcset = chunkID_set_init("size=15,type=bitmap");
-            if(!rcset){
+            if (!rcset) {
                 fprintf(stderr,"Unable to allocate memory for rcset\n");
+
                 return -1;
             }
-            fillChunkID_set(rcset,random_bmap);
+            fillChunkID_set(rcset, random_bmap);
             fprintf(stdout, "2) Message SEND_BMAP: I send my buffer of %d chunks\n", chunkID_set_size(rcset));
             printChunkID_set(rcset);
-            sendBufferMap(remote,my_sock,rcset,trans_id++);
+            sendBufferMap(remote, my_sock, rcset, trans_id++);
             break;
         case sig_request_buffermap:
             fprintf(stdout, "1) Message REQUEST_BMAP: Someone requeste my buffer map [%d]\n", (cset == NULL));
             rcset = chunkID_set_init("size=15,type=bitmap");
-            if(!rcset){
+            if (!rcset) {
                 fprintf(stderr,"Unable to allocate memory for rcset\n");
+
                 return -1;
             }
-            fillChunkID_set(rcset,random_bmap);
-            sendBufferMap(remote,my_sock,rcset,trans_id++);
+            fillChunkID_set(rcset, random_bmap);
+            sendBufferMap(remote, my_sock, rcset, trans_id++);
             break;
     }
     nodeid_free(remote);
     if(owner)nodeid_free(owner);
     if (cset) chunkID_set_free(cset);
     if (rcset) chunkID_set_free(rcset);
-    return 1;
-  }
 
-int main(int argc, char *argv[]) {
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
     struct nodeID *my_sock;    
     int ret;
+
     cmdline_parse(argc, argv);
     my_sock = init();
     ret = 0;
-    if(dst_port!=0)
+    if (dst_port != 0)
         ret = client_side(my_sock);
      else
         ret = server_side(my_sock);//receiver side
     nodeid_free(my_sock);
+
     return ret;
 }
