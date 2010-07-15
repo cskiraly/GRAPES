a1333f467f1b25652d57fb188391b19ba7a69df0
Luca Abeni <luca.abeni@unitn.it>
2010-07-15 14:22:40 +0200

    Make the cache size configurable

--- som/TopologyManager/ncast.c
+++ som/TopologyManager/ncast.c
@@ -17,12 +17,13 @@
 #include "topocache.h"
 #include "topo_proto.h"
 #include "proto.h"
+#include "config.h"
 #include "msg_types.h"
 
-#define MAX_PEERS 10
+#define DEFAULT_CACHE_SIZE 10
 
 static uint64_t currtime;
-static int cache_size = MAX_PEERS;
+static int cache_size;
 static struct peer_cache *local_cache;
 static bool bootstrap = true;
 static int bootstrap_period = 2000000;
@@ -54,6 +55,16 @@ static int time_to_send(void)
  */
 int topInit(struct nodeID *myID, void *metadata, int metadata_size, const char *config)
 {
+  struct tag *cfg_tags;
+  int res;
+
+  cfg_tags = config_parse(config);
+  res = config_value_int(cfg_tags, "cache_size", &cache_size);
+  if (!res) {
+    cache_size = DEFAULT_CACHE_SIZE;
+  }
+  free(cfg_tags);
+  
   local_cache = cache_init(cache_size, metadata_size);
   if (local_cache == NULL) {
     return -1;
