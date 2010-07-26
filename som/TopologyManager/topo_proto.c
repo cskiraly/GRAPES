d0bde46df803951977510ef07760c2f9df10d8d8
Marco Biazzini <biazzini@disi.unitn.it>
2010-07-26 13:50:14 +0200

    Topology : implementing max_gossiping_peers for tman

--- som/TopologyManager/topo_proto.c
+++ som/TopologyManager/topo_proto.c
@@ -18,27 +18,29 @@
 
 static struct peer_cache *myEntry;
 
-static int ncast_payload_fill(uint8_t *payload, int size, struct peer_cache *c, struct nodeID *snot)
+static int ncast_payload_fill(uint8_t *payload, int size, struct peer_cache *c, struct nodeID *snot, int max_peers)
 {
   int i;
   uint8_t *p = payload;
 
+  if (!max_peers) max_peers = MAX_MSG_SIZE; // just to be sure to dump the whole cache...
   p += cache_header_dump(p, c);
   p += entry_dump(p, myEntry, 0);
-  for (i = 0; nodeid(c, i); i++) {
+  for (i = 0; nodeid(c, i) && max_peers; i++) {
     if (!nodeid_equal(nodeid(c, i), snot)) {
       if (p - payload > size - 32 /* FIXME */) {
         fprintf(stderr, "too many entries!\n");
         return -1;
       }
       p += entry_dump(p, c, i);
+      --max_peers;
     }
   }
 
   return p - payload;
 }
 
-static int topo_reply(const struct peer_cache *c, struct peer_cache *local_cache, int protocol, int type)
+static int topo_reply(const struct peer_cache *c, struct peer_cache *local_cache, int protocol, int type, int max_peers)
 {
   uint8_t pkt[MAX_MSG_SIZE];
   struct topo_header *h = (struct topo_header *)pkt;
@@ -55,14 +57,14 @@ static int topo_reply(const struct peer_cache *c, struct peer_cache *local_cache
   dst = nodeid(c, 0);
   h->protocol = protocol;
   h->type = type;
-  len = ncast_payload_fill(pkt + sizeof(struct topo_header), MAX_MSG_SIZE - sizeof(struct topo_header), local_cache, dst);
+  len = ncast_payload_fill(pkt + sizeof(struct topo_header), MAX_MSG_SIZE - sizeof(struct topo_header), local_cache, dst, max_peers);
 
   res = len > 0 ? send_to_peer(nodeid(myEntry, 0), dst, pkt, sizeof(struct topo_header) + len) : len;
 
   return res;
 }
 
-static int topo_query_peer(struct peer_cache *local_cache, struct nodeID *dst, int protocol, int type)
+static int topo_query_peer(struct peer_cache *local_cache, struct nodeID *dst, int protocol, int type, int max_peers)
 {
   uint8_t pkt[MAX_MSG_SIZE];
   struct topo_header *h = (struct topo_header *)pkt;
@@ -70,28 +72,28 @@ static int topo_query_peer(struct peer_cache *local_cache, struct nodeID *dst, i
 
   h->protocol = protocol;
   h->type = type;
-  len = ncast_payload_fill(pkt + sizeof(struct topo_header), MAX_MSG_SIZE - sizeof(struct topo_header), local_cache, dst);
+  len = ncast_payload_fill(pkt + sizeof(struct topo_header), MAX_MSG_SIZE - sizeof(struct topo_header), local_cache, dst, max_peers);
   return len > 0  ? send_to_peer(nodeid(myEntry, 0), dst, pkt, sizeof(struct topo_header) + len) : len;
 }
 
 int ncast_reply(const struct peer_cache *c, struct peer_cache *local_cache)
 {
-  return topo_reply(c, local_cache, MSG_TYPE_TOPOLOGY, NCAST_REPLY);
+  return topo_reply(c, local_cache, MSG_TYPE_TOPOLOGY, NCAST_REPLY, 0);
 }
 
-int tman_reply(const struct peer_cache *c, struct peer_cache *local_cache)
+int tman_reply(const struct peer_cache *c, struct peer_cache *local_cache, int max_peers)
 {
-  return topo_reply(c, local_cache, MSG_TYPE_TMAN, TMAN_REPLY);
+  return topo_reply(c, local_cache, MSG_TYPE_TMAN, TMAN_REPLY, max_peers);
 }
 
 int ncast_query_peer(struct peer_cache *local_cache, struct nodeID *dst)
 {
-  return topo_query_peer(local_cache, dst, MSG_TYPE_TOPOLOGY, NCAST_QUERY);
+  return topo_query_peer(local_cache, dst, MSG_TYPE_TOPOLOGY, NCAST_QUERY, 0);
 }
 
-int tman_query_peer(struct peer_cache *local_cache, struct nodeID *dst)
+int tman_query_peer(struct peer_cache *local_cache, struct nodeID *dst, int max_peers)
 {
-  return topo_query_peer(local_cache, dst, MSG_TYPE_TMAN, TMAN_QUERY);
+  return topo_query_peer(local_cache, dst, MSG_TYPE_TMAN, TMAN_QUERY, max_peers);
 }
 
 int ncast_query(struct peer_cache *local_cache)
@@ -102,7 +104,7 @@ int ncast_query(struct peer_cache *local_cache)
   if (dst == NULL) {
     return 0;
   }
-  return topo_query_peer(local_cache, dst, MSG_TYPE_TOPOLOGY, NCAST_QUERY);
+  return topo_query_peer(local_cache, dst, MSG_TYPE_TOPOLOGY, NCAST_QUERY, 0);
 }
 
 int topo_proto_metadata_update(void *meta, int meta_size)
--- som/TopologyManager/topo_proto.h
+++ som/TopologyManager/topo_proto.h
@@ -1,9 +1,8 @@
 int ncast_reply(const struct peer_cache *c, struct peer_cache *local_cache);
-int tman_reply(const struct peer_cache *c, struct peer_cache *local_cache);
+int tman_reply(const struct peer_cache *c, struct peer_cache *local_cache, int max_peers);
 int ncast_query(struct peer_cache *local_cache);
 int tman_query(struct peer_cache *local_cache);
-int tman_query_peer(struct peer_cache *local_cache, struct nodeID *dst);
+int tman_query_peer(struct peer_cache *local_cache, struct nodeID *dst, int max_peers);
 int ncast_query_peer(struct peer_cache *local_cache, struct nodeID *dst);
-//int topo_query_peer(struct peer_cache *local_cache, struct nodeID *dst);
 int topo_proto_metadata_update(void *meta, int meta_size);
 int topo_proto_init(struct nodeID *s, void *meta, int meta_size);
