babdcc03fa4e5fc8b0e9d214be17993ea5f29afd
Luca Abeni <luca.abeni@unitn.it>
2010-12-02 09:20:23 +0100

    Cosmetic fix (position of "{")

--- src/PeerSampler/ncast.c
+++ src/PeerSampler/ncast.c
@@ -44,7 +44,8 @@ static uint64_t gettime(void)
   return tv.tv_usec + tv.tv_sec * 1000000ull;
 }
 
-static struct peersampler_context* ncast_context_init(void){
+static struct peersampler_context* ncast_context_init(void)
+{
   struct peersampler_context* con;
   con = (struct peersampler_context*) calloc(1,sizeof(struct peersampler_context));
 
