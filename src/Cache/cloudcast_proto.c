e910cf6cb6ecedfe4b60b8c17f8bc8e648f71064
Andrea Zito <zito.andrea@gmail.com>
2011-04-23 10:27:15 +0200

    Major rewrite of cloudact protocol. Current implementation diverge from present cyclon implementation and is reflect more strictly the paper description.

--- src/Cache/cloudcast_proto.c
+++ src/Cache/cloudcast_proto.c
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "net_helper.h"
 #include "topocache.h"
@@ -19,8 +20,10 @@
 #define MAX_MSG_SIZE 1500
 
 #define CLOUD_VIEW_KEY "view"
+#define CLOUDCAST_MESSAGE_HEADER 10 /* PROTOCOL MSG_TYPE LAST_CLOUD_CONTACT */
 
-struct topo_header cloud_header = {MSG_TYPE_TOPOLOGY, CLOUDCAST_CLOUD};
+uint8_t cloud_header[10] = { MSG_TYPE_TOPOLOGY, CLOUDCAST_CLOUD, 0llu};
+
 struct cloudcast_proto_context {
   struct peer_cache* myEntry;
   uint8_t def_cloudcache[128];
@@ -88,14 +91,18 @@ int cloudcast_is_cloud_node(struct cloudcast_proto_context *con, struct nodeID* 
   return is_cloud_node(con->cloud_context, node);
 }
 
-int cloudcast_reply_peer(struct cloudcast_proto_context *context, const struct peer_cache *c, struct peer_cache *local_cache)
+int cloudcast_reply_peer(struct cloudcast_proto_context *context, const struct peer_cache *c, struct peer_cache *local_cache, uint64_t last_cloud_contact_sec)
 {
-  return topo_reply(context->topo_context, c, local_cache, MSG_TYPE_TOPOLOGY, CLOUDCAST_REPLY, 0, 0);
+  uint8_t *header = (uint8_t *) &last_cloud_contact_sec;
+  int header_len = sizeof(uint64_t);
+  return topo_reply_header(context->topo_context, c, local_cache, MSG_TYPE_TOPOLOGY, CLOUDCAST_REPLY, header, header_len, 0, 0);
 }
 
-int cloudcast_query_peer(struct cloudcast_proto_context *context, struct peer_cache *sent_cache, struct nodeID *dst)
+int cloudcast_query_peer(struct cloudcast_proto_context *context, struct peer_cache *sent_cache, struct nodeID *dst, uint64_t last_cloud_contact_sec)
 {
-  return topo_query_peer(context->topo_context, sent_cache, dst, MSG_TYPE_TOPOLOGY, CLOUDCAST_QUERY, 0);
+  uint8_t *header = (uint8_t *) &last_cloud_contact_sec;
+  int header_len = sizeof(uint64_t);
+  return topo_query_peer_header(context->topo_context, sent_cache, dst, MSG_TYPE_TOPOLOGY, CLOUDCAST_QUERY, header, header_len, 0);
 }
 
 
@@ -104,7 +111,7 @@ int cloudcast_payload_fill(struct cloudcast_proto_context *context, uint8_t *pay
   int i;
   uint8_t *p = payload;
 
-  if (!max_peers) max_peers = MAX_MSG_SIZE; // just to be sure to dump the whole cache...
+  if (!max_peers) max_peers = MAX_MSG_SIZE; // FIXME: we should use topo_proto for this
   p += cache_header_dump(p, c, include_me);
   if (include_me) {
     p += entry_dump(p, context->myEntry, 0, size - (p - payload));
@@ -129,10 +136,10 @@ int cloudcast_payload_fill(struct cloudcast_proto_context *context, uint8_t *pay
 
 int cloudcast_reply_cloud(struct cloudcast_proto_context *context, struct peer_cache *cloud_cache)
 {
-  uint8_t headerless_pkt[MAX_MSG_SIZE - sizeof(cloud_header)];
+  uint8_t headerless_pkt[MAX_MSG_SIZE - CLOUDCAST_MESSAGE_HEADER];
   int len, res;
 
-  len = cloudcast_payload_fill(context, headerless_pkt, MAX_MSG_SIZE - sizeof(cloud_header), cloud_cache, 0, 1);
+  len = cloudcast_payload_fill(context, headerless_pkt, MAX_MSG_SIZE - CLOUDCAST_MESSAGE_HEADER, cloud_cache, 0, 1);
 
   if (len > 0)
     res = put_on_cloud(context->cloud_context, CLOUD_VIEW_KEY, headerless_pkt, len, 0);
@@ -144,7 +151,7 @@ int cloudcast_reply_cloud(struct cloudcast_proto_context *context, struct peer_c
 int cloudcast_query_cloud(struct cloudcast_proto_context *context)
 {
   return get_from_cloud_default(context->cloud_context, CLOUD_VIEW_KEY,
-                                (uint8_t *)&cloud_header, sizeof(cloud_header),
+                                cloud_header, CLOUDCAST_MESSAGE_HEADER,
                                 0, context->def_cloudcache,
                                 context->def_cloudcache_len, 0);
 }
