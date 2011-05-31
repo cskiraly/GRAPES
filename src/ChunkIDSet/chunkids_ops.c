1c063cb3617b283eedbc639154ed626b589f6b74
Luca Abeni <luca.abeni@unitn.it>
2011-05-31 13:57:22 +0200

    Split the set and list operations in different files

--- src/ChunkIDSet/chunkids_ops.c
+++ src/ChunkIDSet/chunkids_ops.c
@@ -16,6 +16,12 @@
 
 #define DEFAULT_SIZE_INCREMENT 32
 
+int chunkID_set_add_chunk_list(struct chunkID_set *h, int chunk_id);
+inline int chunkID_set_check_list(const struct chunkID_set *h, int chunk_id);
+int chunkID_set_add_chunk_set(struct chunkID_set *h, int chunk_id);
+inline int chunkID_set_check_set(const struct chunkID_set *h, int chunk_id);
+
+
 struct chunkID_set *chunkID_set_init(const char *config)
 {
   struct chunkID_set *p;
@@ -65,90 +71,6 @@ struct chunkID_set *chunkID_set_init(const char *config)
   return p;
 }
 
-static int chunkID_set_add_chunk_list(struct chunkID_set *h, int chunk_id)
-{
-  if (chunkID_set_check(h, chunk_id) >= 0) {
-    return 0;
-  }
-
-  if (h->n_elements == h->size) {
-    int *res;
-
-    res = realloc(h->elements, (h->size + DEFAULT_SIZE_INCREMENT) * sizeof(int));
-    if (res == NULL) {
-      return -1;
-    }
-    h->size += DEFAULT_SIZE_INCREMENT;
-    h->elements = res;
-  }
-  h->elements[h->n_elements++] = chunk_id;
-
-  return h->n_elements;
-}
-
-static int int_cmp(const void *pa, const void *pb)
-{
-  return (*(const int *)pa - *(const int *)pb);
-}
-
-static int check_insert_pos(const struct chunkID_set *h, int id)
-{
-  int a, b, c, r;
-
-  if (! h->n_elements) {
-    return 0;
-  }
-
-  a = 0;
-  b = c = h->n_elements - 1;
-
-  while ((r = int_cmp(&id, &h->elements[b])) != 0) {
-    if (r > 0) {
-      if (b == c) {
-        return b + 1;
-      } else {
-        a = b + 1;
-      }
-    } else {
-      if (b == a) {
-        return b;
-      } else {
-        c = b;
-      }
-    }
-    b = (a + c) / 2;
-  }
-
-  return -1;
-}
-
-static int chunkID_set_add_chunk_set(struct chunkID_set *h, int chunk_id)
-{
-  int pos;
-
-  pos = check_insert_pos(h, chunk_id);
-  if (pos < 0){
-    return 0;
-  }
-
-  if (h->n_elements == h->size) {
-    int *res;
-
-    res = realloc(h->elements, (h->size + DEFAULT_SIZE_INCREMENT) * sizeof(int));
-    if (res == NULL) {
-      return -1;
-    }
-    h->size += DEFAULT_SIZE_INCREMENT;
-    h->elements = res;
-  }
-
-  memmove(&h->elements[pos + 1], &h->elements[pos] , ((h->n_elements++) - pos) * sizeof(int));
-
-  h->elements[pos] = chunk_id;
-
-  return h->n_elements;
-}
-
 int chunkID_set_add_chunk(struct chunkID_set *h, int chunk_id)
 {
   if (h->type == CIST_BITMAP) {
@@ -168,27 +90,6 @@ int chunkID_set_get_chunk(const struct chunkID_set *h, int i)
 {
   if (i < h->n_elements) {
     return h->elements[i];
-  }
-
-  return -1;
-}
-
-static inline int chunkID_set_check_set(const struct chunkID_set *h, int chunk_id)
-{
-  int *p;
-
-  p = bsearch(&chunk_id, h->elements, (size_t) h->n_elements, sizeof(h->elements[0]), int_cmp);
-  return p ? p - h->elements : -1;
-}
-
-static inline int chunkID_set_check_list(const struct chunkID_set *h, int chunk_id)
-{
-  int i;
-
-  for (i = 0; i < h->n_elements; i++) {
-    if (h->elements[i] == chunk_id) {
-      return i;
-    }
   }
 
   return -1;
---
+++ src/ChunkIDSet/chunkids_ops_list.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <limits.h>
+#include <string.h>
+#include <assert.h>
+
+#include "chunkids_private.h"
+
+#define DEFAULT_SIZE_INCREMENT 32
+
+int chunkID_set_check_list(const struct chunkID_set *h, int chunk_id)
+{
+  int i;
+
+  for (i = 0; i < h->n_elements; i++) {
+    if (h->elements[i] == chunk_id) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+int chunkID_set_add_chunk_list(struct chunkID_set *h, int chunk_id)
+{
+  if (chunkID_set_check_list(h, chunk_id) >= 0) {
+    return 0;
+  }
+
+  if (h->n_elements == h->size) {
+    int *res;
+
+    res = realloc(h->elements, (h->size + DEFAULT_SIZE_INCREMENT) * sizeof(int));
+    if (res == NULL) {
+      return -1;
+    }
+    h->size += DEFAULT_SIZE_INCREMENT;
+    h->elements = res;
+  }
+  h->elements[h->n_elements++] = chunk_id;
+
+  return h->n_elements;
+}
