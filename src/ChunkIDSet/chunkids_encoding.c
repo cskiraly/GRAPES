0c8d7f4ba6cd76a082e6e5213b42160f0ce7f6c8
Luca <luca@luca.(none)>
2011-05-31 11:59:00 +0200

    split some functions

--- src/ChunkIDSet/chunkids_encoding.c
+++ src/ChunkIDSet/chunkids_encoding.c
@@ -14,9 +14,90 @@
 #include "trade_sig_la.h"
 #include "int_coding.h"
 
-int encodeChunkSignaling(const struct chunkID_set *h, const void *meta, int meta_len, uint8_t *buff, int buff_len)
+static uint8_t *bmap_encode(const struct chunkID_set *h, uint8_t *buff, int buff_len, int meta_len)
+{
+  int i, elements;
+  uint32_t c_min, c_max;
+
+  c_min = c_max = h->n_elements ? h->elements[0] : 0;
+  for (i = 1; i < h->n_elements; i++) {
+    if (h->elements[i] < c_min)
+      c_min = h->elements[i];
+    else if (h->elements[i] > c_max)
+      c_max = h->elements[i];
+  }
+  elements = h->n_elements ? c_max - c_min + 1 : 0;
+  int_cpy(buff, elements);
+  elements = elements / 8 + (elements % 8 ? 1 : 0);
+  if (buff_len < elements + 16 + meta_len) {
+    return NULL;
+  }
+  int_cpy(buff + 12, c_min); //first value in the bitmap, i.e., base value
+  memset(buff + 16, 0, elements);
+  for (i = 0; i < h->n_elements; i++) {
+    buff[16 + (h->elements[i] - c_min) / 8] |= 1 << ((h->elements[i] - c_min) % 8);
+  }
+
+  return buff + 16 + elements;
+}
+
+static uint8_t *prio_encode(const struct chunkID_set *h, uint8_t *buff, int buff_len, int meta_len)
 {
   int i;
+
+  int_cpy(buff, h->n_elements);
+  if (buff_len < h->n_elements * 4 + 12 + meta_len) {
+    return NULL;
+  }
+  for (i = 0; i < h->n_elements; i++) {
+    int_cpy(buff + 12 + i * 4, h->elements[i]);
+  }
+
+  return buff + 12 + h->n_elements * 4;
+}
+
+const uint8_t *bmap_decode(struct chunkID_set *h, const uint8_t *buff, int buff_len, int *meta_len)
+{
+  int i;
+  int base;
+  int byte_cnt;
+
+  byte_cnt = h->size / 8 + (h->size % 8 ? 1 : 0);
+  if (buff_len < 16 + byte_cnt + *meta_len) {
+    fprintf(stderr, "Error in decoding chunkid set - wrong length\n");
+    chunkID_set_free(h);
+
+    return NULL;
+  }
+  base = int_rcpy(buff + 12);
+  for (i = h->size - 1; i >= 0; i--) {
+  if (buff[16 + (i / 8)] & 1 << (i % 8))
+    h->elements[h->n_elements++] = base + i;
+  }
+
+  return buff + 16 + byte_cnt;
+}
+
+const uint8_t *prio_decode(struct chunkID_set *h, const uint8_t *buff, int buff_len, int *meta_len)
+{
+  int i;
+
+  if (buff_len != h->size * 4 + 12 + *meta_len) {
+    fprintf(stderr, "Error in decoding chunkid set - wrong length.\n");
+    chunkID_set_free(h);
+
+    return NULL;
+  }
+  for (i = 0; i < h->size; i++) {
+    h->elements[i] = int_rcpy(buff + 12 + i * 4);
+  }
+  h->n_elements = h->size;
+
+  return buff + 12 + h->size * 4;
+}
+
+int encodeChunkSignaling(const struct chunkID_set *h, const void *meta, int meta_len, uint8_t *buff, int buff_len)
+{
   uint8_t *meta_p;
   uint32_t type = h ? h->type : -1;
   
@@ -25,41 +106,10 @@ int encodeChunkSignaling(const struct chunkID_set *h, const void *meta, int meta
 
   switch (type) {
     case CIST_BITMAP:
-    {
-      int elements;
-      uint32_t c_min, c_max;
-
-      c_min = c_max = h->n_elements ? h->elements[0] : 0;
-      for (i = 1; i < h->n_elements; i++) {
-        if (h->elements[i] < c_min)
-          c_min = h->elements[i];
-        else if (h->elements[i] > c_max)
-          c_max = h->elements[i];
-      }
-      elements = h->n_elements ? c_max - c_min + 1 : 0;
-      int_cpy(buff, elements);
-      elements = elements / 8 + (elements % 8 ? 1 : 0);
-      if (buff_len < elements + 16 + meta_len) {
-        return -1;
-      }
-      int_cpy(buff + 12, c_min); //first value in the bitmap, i.e., base value
-      memset(buff + 16, 0, elements);
-      for (i = 0; i < h->n_elements; i++) {
-        buff[16 + (h->elements[i] - c_min) / 8] |= 1 << ((h->elements[i] - c_min) % 8);
-      }
-      meta_p = buff + 16 + elements;
+      meta_p =  bmap_encode(h, buff, buff_len, meta_len);
       break;
-    }
     case CIST_PRIORITY:
-      int_cpy(buff, h->n_elements);
-      if (buff_len < h->n_elements * 4 + 12 + meta_len) {
-        return -1;
-      }
-      for (i = 0; i < h->n_elements; i++) {
-        int_cpy(buff + 12 + i * 4, h->elements[i]);
-      }
-      meta_p = buff + 12 + h->n_elements * 4;
-
+      meta_p =  prio_encode(h, buff, buff_len, meta_len);
       break;
     case -1:
       int_cpy(buff, 0);
@@ -69,6 +119,9 @@ int encodeChunkSignaling(const struct chunkID_set *h, const void *meta, int meta
       fprintf(stderr, "Invalid ChunkID encoding type %d\n", type);
 
       return -1;
+  }
+  if (meta_p == NULL) {
+    return -1;
   }
 
   if (meta_len) {
@@ -80,7 +133,6 @@ int encodeChunkSignaling(const struct chunkID_set *h, const void *meta, int meta
 
 struct chunkID_set *decodeChunkSignaling(void **meta, int *meta_len, const uint8_t *buff, int buff_len)
 {
-  int i;
   uint32_t size;
   uint32_t type;
   struct chunkID_set *h;
@@ -108,38 +160,10 @@ struct chunkID_set *decodeChunkSignaling(void **meta, int *meta_len, const uint8
 
   switch (type) {
     case CIST_BITMAP:
-    {
-      // uint8_t bitmap;
-      int base;
-      int byte_cnt;
-
-      byte_cnt = size / 8 + (size % 8 ? 1 : 0);
-      if (buff_len < 16 + byte_cnt + *meta_len) {
-        fprintf(stderr, "Error in decoding chunkid set - wrong length\n");
-        chunkID_set_free(h);
-
-        return NULL;
-      }
-      base = int_rcpy(buff + 12);
-      for (i = size - 1; i >= 0; i--) {
-        if (buff[16 + (i / 8)] & 1 << (i % 8))
-          h->elements[h->n_elements++] = base + i;
-      }
-      meta_p = buff + 16 + byte_cnt;
+      meta_p = bmap_decode(h, buff, buff_len, meta_len);
       break;
-    }
     case CIST_PRIORITY:
-      if (buff_len != size * 4 + 12 + *meta_len) {
-        fprintf(stderr, "Error in decoding chunkid set - wrong length.\n");
-        chunkID_set_free(h);
-
-        return NULL;
-      }
-      for (i = 0; i < size; i++) {
-        h->elements[i] = int_rcpy(buff + 12 + i * 4);
-      }
-      h->n_elements = size;
-      meta_p = buff + 12 + size * 4;
+      meta_p = prio_decode(h, buff, buff_len, meta_len);
       break;
     case -1:
       meta_p = buff + 12;
