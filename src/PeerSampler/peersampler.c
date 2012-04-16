54cb739a3010b40b541b28ce7337803cc1e4d0fe
Csaba Kiraly <kiraly@disi.unitn.it>
2012-04-17 00:42:15 +0200

    change default peer sampler to newscastplus

--- src/PeerSampler/peersampler.c
+++ src/PeerSampler/peersampler.c
@@ -29,7 +29,7 @@ struct psample_context* psample_init(struct nodeID *myID, const void *metadata, 
   tc = malloc(sizeof(struct psample_context));
   if (!tc) return NULL;
 
-  tc->ps = &cyclon;
+  tc->ps = &ncastplus;
   cfg_tags = config_parse(config);
   proto = config_value_str(cfg_tags, "protocol");
   if (proto) {
