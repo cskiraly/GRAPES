cskiraly/GRAPES

commit e44ff2bb18f887791586d8eff57e78af53756650

making the UDP chunkiser Win32 compatible

src/Chunkiser/input-stream-udp.c
@@ -4,8 +4,12 @@
  *  This is free software; see gpl-3.0.txt
  */
 #include <sys/time.h>
+#ifndef _WIN32
 #include <sys/socket.h>
 #include <netinet/in.h>
+#else
+#include <winsock2.h>
+#endif
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -36,7 +40,7 @@ static int input_get_udp(uint8_t *data, int fd)
 {
   ssize_t msglen;
 
-  msglen = recv(fd, data, UDP_BUF_SIZE, MSG_DONTWAIT);
+  msglen = recv(fd, data, UDP_BUF_SIZE, 0);
   if (msglen <= 0) {
     return 0;
   }
@@ -54,6 +58,15 @@ static int listen_udp(int port)
   if (fd < 0) {
     return -1;
   }
+
+#ifndef _WIN32
+  fcntl(fd, F_SETFL, O_NONBLOCK);
+#else
+  {
+    unsigned long nonblocking = 1;
+    ioctlsocket(fd, FIONBIO, (unsigned long*) &nonblocking);
+  }
+#endif
 
   memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;

src/Chunkiser/output-stream-udp.c
@@ -3,9 +3,13 @@
  *
  *  This is free software; see gpl-3.0.txt
  */
+#ifndef _WIN32
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#else
+#include <winsock2.h>
+#endif
 #include <unistd.h>
 #include <stdint.h>
 #include <limits.h>
@@ -19,6 +23,19 @@
 #include "dechunkiser_iface.h"
 
 #define UDP_PORTS_NUM_MAX 10
+
+#ifdef _WIN32
+static int inet_aton(const char *cp, struct in_addr *addr)
+{
+    if( cp==NULL || addr==NULL )
+    {
+        return(0);
+    }
+
+    addr->s_addr = inet_addr(cp);
+    return (addr->s_addr == INADDR_NONE) ? 0 : 1;
+}
+#endif
 
 struct dechunkiser_ctx {
   int outfd;

commit b63a6c8a597331985ba40f9df7f3f34b5f483b95

fix input-stream-udp on Linux
regression from e44ff2bb18f887791586d8eff57e78af53756650

src/Chunkiser/input-stream-udp.c
@@ -7,6 +7,7 @@
 #ifndef _WIN32
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <fcntl.h>
 #else
 #include <winsock2.h>
 #endif