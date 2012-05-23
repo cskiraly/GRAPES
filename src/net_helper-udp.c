09f6f5efef02c55ec685eed7d5c723df431eeb1f
Luca Abeni <luca.abeni@unitn.it>
2012-05-23 11:47:22 +0200

    Remove static structures

--- src/net_helper-udp.c
+++ src/net_helper-udp.c
@@ -248,7 +248,7 @@ struct my_hdr_t {
 int send_to_peer(const struct nodeID *from, struct nodeID *to, const uint8_t *buffer_ptr, int buffer_size)
 {
   struct msghdr msg = {0};
-  static struct my_hdr_t my_hdr;
+  struct my_hdr_t my_hdr  = {0};
   struct iovec iov[2];
   int res;
 
@@ -290,7 +290,7 @@ int recv_from_peer(const struct nodeID *local, struct nodeID **remote, uint8_t *
   int res, recv, m_seq, frag_seq;
   struct sockaddr_in raddr;
   struct msghdr msg = {0};
-  static struct my_hdr_t my_hdr;
+  struct my_hdr_t my_hdr = {0};
   struct iovec iov[2];
 
   iov[0].iov_base = &my_hdr;
